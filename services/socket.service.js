2f4352611d4f2b9f4be9eaab3784909c851918a1
Ofir-Roz <ofir19923@gmail.com>
2025-06-02 16:49:30 +0300

    Refactor socket event handling and improve board update logging

--- services/socket.service.js
+++ services/socket.service.js
@@ -12,6 +12,10 @@ export function setupSocketAPI(server) {
     gIo.on('connection', socket => {
         loggerService.info(`New connected socket [id: ${socket.id}]`)
         
+        socket.on('disconnect', () => {
+            loggerService.info(`Socket disconnected [id: ${socket.id}]`)
+        })
+        
         socket.on('join-board', boardId => {
             socket.join('board:' + boardId)
             loggerService.info(`Socket ${socket.id} joined board:${boardId}`)
@@ -21,10 +25,25 @@ export function setupSocketAPI(server) {
             socket.leave('board:' + boardId)
             loggerService.info(`Socket ${socket.id} left board:${boardId}`)
         })
+        
+        socket.on('set-user-socket', userId => {
+            loggerService.info(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
+            socket.userId = userId
+        })
+        
+        socket.on('unset-user-socket', () => {
+            loggerService.info(`Removing socket.userId for socket [id: ${socket.id}]`)
+            delete socket.userId
+        })
     })
 }
 
 export function emitBoardUpdate(boardId, updatedBoard) {
-    if (!gIo) return
-    gIo.to('board:' + boardId).emit('board-updated', updatedBoard)
+    if (!gIo) {
+        console.log('No socket.io instance available')
+        return
+    }
+    const roomName = 'board:' + boardId
+    console.log(`Emitting board-updated to room: ${roomName}`)
+    gIo.to(roomName).emit('board-updated', updatedBoard)
 }
