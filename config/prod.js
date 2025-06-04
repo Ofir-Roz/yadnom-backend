33a47803de684695c650b0594818ee49316ade8f
Ofir-Roz <ofir19923@gmail.com>
2025-06-04 21:37:27 +0300

    Update database connection URL to use environment variable to hide secrets

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://anistu:WGvRd3jFNR8tzi3w@cluster0.uqvtq.mongodb.net/',
+    dbURL: process.env.MONGO_URL ,
     dbName: process.env.DB_NAME || 'yadnom-data'
 }
